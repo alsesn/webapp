0c63647c54f913e006c35d9d4835a7a4b6eb3fe4
alesn <rostislavsusak747@gmai.com>
2024-09-08 13:01:59 +0300

    update

--- src/main/java/me/alsesn/webapp/model/ClientDto.java
+++ src/main/java/me/alsesn/webapp/model/ClientDto.java
@@ -1,4 +1,25 @@
 package me.alsesn.webapp.model;
 
-public class ClientDetail {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+
+@Data
+public class ClientDto {
+    @NotEmpty(message = "The First Name is required")
+    private String firstName;
+
+    @NotEmpty(message = "The Last Name is required")
+    private String lastName;
+
+    @NotEmpty(message = "The email is required")
+    @Email
+    private String email;
+
+    private String phone;
+    private String address;
+
+    @NotEmpty(message = "The Status is required")
+    private String status;
 }
